42490f147c37dbf4c1e4ce6c008ebe416f627f63
janeG <jane_g@outlook.com>
2017-08-16 22:52:28 +1000

    Fix paint calculator

--- DebuggingExercises/PaintCalculator.java
+++ DebuggingExercises/PaintCalculator.java
@@ -13,7 +13,8 @@ public class PaintCalculator
         //length = getInput("length");
         //width = getInput("width");
         //height = getInput("height");
-        double area = calculateArea(height, width);
+        double area= calculateTotalArea(height, width, length);
+
         double gallon = calculateGallon(area);
         double cost = calculateCost(gallon);
         System.out.print(String.format("The cost of paint a %.0f-by-%.0f-foot room with %.0f-foot ceiling is $%.2f"
@@ -29,10 +30,10 @@ public class PaintCalculator
         userInput = input.nextDouble();
         return userInput;
     }
-    private static double calculateArea(double h, double w)
+    private static double calculateTotalArea(double h, double w, double l)
     {
         double area;
-        area = h*w;
+        area = ((h * w)+(h * l))*2;
         return area;
     }
     private static double calculateGallon(double a)
