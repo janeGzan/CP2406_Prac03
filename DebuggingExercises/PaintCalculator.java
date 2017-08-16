ab99d293589cea1927eba6d6ec98bdd2cdd4ad41
jc429278 <jane.ganzan@my.jcu.edu.au>
2017-08-16 10:32:12 +1000

    Finish Paint Calculator Exercise

---
+++ DebuggingExercises/PaintCalculator.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+/*
+ Created by jc429278 on 16/08/17.
+ */
+public class PaintCalculator
+{
+    public static void main(String args[]) {
+        double length = 15;
+        double width = 20;
+        double height = 10;
+
+        //length = getInput("length");
+        //width = getInput("width");
+        //height = getInput("height");
+        double area = calculateArea(height, width);
+        double gallon = calculateGallon(area);
+        double cost = calculateCost(gallon);
+        System.out.print(String.format("The cost of paint a %.0f-by-%.0f-foot room with %.0f-foot ceiling is $%.2f"
+                , length, width, height, cost ));
+
+    }
+
+    private static double getInput(String type)
+    {
+        double userInput;
+        System.out.print(String.format("Enter %s:", type));
+        Scanner input = new Scanner(System.in);
+        userInput = input.nextDouble();
+        return userInput;
+    }
+    private static double calculateArea(double h, double w)
+    {
+        double area;
+        area = h*w;
+        return area;
+    }
+    private static double calculateGallon(double a)
+    {
+        double numberOfGallons;
+        double AREACOVERED = 350;
+        numberOfGallons = a/AREACOVERED;
+        numberOfGallons = Math.ceil(numberOfGallons);
+        System.out.println(numberOfGallons);
+        System.out.println(String.format("%.0f Gallons of paint needed", numberOfGallons));
+        return numberOfGallons;
+    }
+    private static double calculateCost(double g)
+    {
+        double PRICE = 32.0;
+        return g * PRICE;
+    }
+
+}
+
